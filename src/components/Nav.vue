2d905649375dc8bee22a2a32a5e2e55ee7a794af
JC Smiley <jcsmileyjr@gmail.com>
2019-11-17 15:42:11 -0600

    Init start files for application

---
+++ src/components/Nav.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="nav-section">
+        <ul class="horizontal-navbar">
+            <li>Frontend Full Stack Job Application</li>
+            <li>Section: Personal</li>
+            <li>1 of 3</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"nav-section"
+}
+</script>
+
+<style scoped>
+    .horizontal-navbar {
+        display: inline-block;
+
+    }
+
+    ul {
+        list-style-type: none;
+    }
+
+    li {
+        float: left;
+    }
+</style>
