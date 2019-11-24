aacde48bea1aa75ea41cfe6eaeee64a1bf71e05f
JC Smiley <jcsmileyjr@gmail.com>
2019-11-23 18:28:58 -0600

    Update Results Page Content

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -2,30 +2,30 @@
     <div id="results">        
         <section>     
             <h3>Personal Information</h3>       
-            <b-row><label>{{personal.fullName}}</label></b-row>
-            <b-row><label>{{personal.homeAddress}}</label></b-row>
-            <b-row><label>{{personal.homeCity}}</label></b-row>
-            <b-row><label>{{personal.homeZipCode}}</label></b-row>
-            <b-row><label>{{personal.homeNumber}}</label></b-row>
-            <b-row><label>{{personal.homeEmail}}</label></b-row>
-            <b-row><label>{{personal.homeDegree}}</label></b-row>                                    
+            <b-row><label><span>Full Name:</span> {{personal.fullName}}</label></b-row>
+            <b-row><label><span>Address:</span> {{personal.homeAddress}}</label></b-row>
+            <b-row><label><span>City: </span> {{personal.homeCity}}</label></b-row>
+            <b-row><label><span>Zip Code:</span> {{personal.homeZipCode}}</label></b-row>
+            <b-row><label><span>Phone Number:</span> {{personal.homeNumber}}</label></b-row>
+            <b-row><label><span>Email:</span> {{personal.homeEmail}}</label></b-row>
+            <b-row><label><span>College Degree:</span> {{personal.homeDegree}}</label></b-row>                                    
         </section>
         <section>     
             <h3>Work History</h3>       
-            <b-row><label>{{work.jobName}}</label></b-row>
-            <b-row><label>{{work.jobAddress}}</label></b-row>
-            <b-row><label>{{work.jobCity}}</label></b-row>
-            <b-row><label>{{work.jobZipCode}}</label></b-row>
-            <b-row><label>{{work.jobTitle}}</label></b-row>
-            <b-row><label>{{work.jobDescr}}</label></b-row>
-            <b-row><label>{{work.jobPay}}</label></b-row>                                    
+            <b-row><label><span>Current Job:</span> {{work.jobName}}</label></b-row>
+            <b-row><label><span>Address:</span> {{work.jobAddress}}</label></b-row>
+            <b-row><label><span>City:</span> {{work.jobCity}}</label></b-row>
+            <b-row><label><span>Zip Code:</span> {{work.jobZipCode}}</label></b-row>
+            <b-row><label><span>Job Title:</span> {{work.jobTitle}}</label></b-row>
+            <b-row><label><span>Description:</span> {{work.jobDescr}}</label></b-row>
+            <b-row><label><span>Pay:</span> {{work.jobPay}}</label></b-row>                                    
         </section>
         <section>     
             <h3>Professional Skills</h3>       
-            <b-row><label>{{skills.feSkills}}</label></b-row>
-            <b-row><label>{{skills.beSkills}}</label></b-row>
-            <b-row><label>{{skills.dbSkills}}</label></b-row>
-            <b-row><label>{{skills.doSkills}}}</label></b-row>                                    
+            <b-row><label><span>Skill #1:</span> {{skills.feSkills}}</label></b-row>
+            <b-row><label><span>Skill #2:</span> {{skills.beSkills}}</label></b-row>
+            <b-row><label><span>Skill #3:</span> {{skills.dbSkills}}</label></b-row>
+            <b-row><label><span>Skill #4:</span> {{skills.doSkills}}}</label></b-row>                                    
         </section>                  
     </div>
 </template>
@@ -47,5 +47,13 @@ export default {
         background-color: white;
         padding: 2rem;
         margin-bottom: 1rem;
+    }
+
+    h3 {
+        text-decoration-line: underline;
+    }
+
+    span {
+        font-weight: 500;
     }
 </style>
